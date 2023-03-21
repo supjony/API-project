a95cf8adeb4efe6707d0ad0ab3fe95d762fee784
Jonathan Sarceno <johnny@Jonathans-MacBook-Air.local>
2023-03-21 14:05:05 -0700

    added seeders to spots

---
+++ backend/db/seeders/20230321205717-demo-spots.js
@@ -0,0 +1,59 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Spots';
+    return queryInterface.bulkInsert(options, [
+      {
+        ownerId: 1,
+        adress: '1 mission st',
+        city: "San francisco 1",
+        state: "CA 1",
+        country: "USA 1",
+        lat: 11.1111111,
+        lng: 111.1111111,
+        name: 'Beaux 1',
+        description: 'it is lit 1',
+        price: 1
+      },
+      {
+        ownerId: 2,
+        adress: '2 mission st',
+        city: "San francisco 2",
+        state: "CA 2",
+        country: "USA 2",
+        lat: 22.2222222,
+        lng: 222.2222222,
+        name: 'Beaux 2',
+        description: 'it is lit 2',
+        price: 2
+      },
+      {
+        ownerId: 3,
+        adress: '3 mission st',
+        city: "San francisco 3",
+        state: "CA 3",
+        country: "USA 3",
+        lat: 33.3333333,
+        lng: 333.3333333,
+        name: 'Beaux 3',
+        description: 'it is lit 3',
+        price: 3
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Spots';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      price: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
