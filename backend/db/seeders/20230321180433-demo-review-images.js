7a2e8b2f587a6b7950dbbc47f8d2157568925d5d
Jonathan Sarceno <johnny@Jonathans-MacBook-Air.local>
2023-03-21 13:52:41 -0700

    added seeder to bookings

---
+++ backend/db/seeders/20230321180433-demo-review-images.js
@@ -0,0 +1,35 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'ReviewImages';
+    return queryInterface.bulkInsert(options, [
+      {
+        reviewId: 1,
+        url: 'url1.com'
+      },
+      {
+        reviewId: 2,
+        url: 'url2.com'
+      },
+      {
+        reviewId: 3,
+        url: 'url3.com'
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'ReviewImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      url: { [Op.in]: ['url1.com', 'url2.com', 'url3.com'] }
+    }, {});
+  }
+};
