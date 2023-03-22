ae5326edf6bf95faa9c69b8b31f1fe2ad44d8262
Jonathan Sarceno <johnny@Jonathans-MacBook-Air.local>
2023-03-22 10:13:03 -0700

    added seeders to review images

---
+++ backend/db/migrations/20230322171054-create-review-image.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('ReviewImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        references: {model: "Reviews"},
+
+      },
+      url: {
+        type: Sequelize.STRING(30)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = "ReviewImages";
+    return queryInterface.dropTable(options);
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// async down(queryInterface, Sequelize) {
+//   // await queryInterface.dropTable('ReviewImages');
+//   options.tableName = "ReviewImages";
+//   return queryInterface.dropTable(options);
+// }
