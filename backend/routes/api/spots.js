81925866c1ed7eac005cd264a56d0528278b4f90
Jonathan Sarceno <johnny@Jonathans-MacBook-Air.local>
2023-06-13 15:22:58 -0700

    get spots of current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,7 +12,70 @@ const user = require('../../db/models/user');
 const router = express.Router();
 
 
+router.get('/current', requireAuth, async (req, res, next) => {
 
+    let currentUserLoggedIn = req.user.id
+
+    const spots = await Spot.findAll({
+        where: {
+            ownerId: currentUserLoggedIn
+        }
+    })
+    const payload = []
+    spotsObj = {}
+    for (let i = 0; i < spots.length; i++) {
+        const spot = spots[i];
+        // const reviews = await spot.getReviews()
+        // for (let i = 0; i < reviews.length; i++) {
+        //     const review = reviews[i];
+        const aggregates = {};
+        const reviews = await Review.findOne({
+            attributes: [
+                [
+                    sequelize.fn('AVG', sequelize.col('stars')),
+                    'avgRating'
+                ]
+            ],
+            where: { spotId: spot.id }
+        });
+
+
+aggregates.reviews = reviews.toJSON().avgRating;
+
+        const previewImage = await SpotImage.findOne({
+            attributes: ['url'],
+            where: { spotId: spot.id }
+        })
+
+
+
+        const spotData = {
+
+            id: spot.id,
+            ownerId: spot.ownerId,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt,
+            avgRating: aggregates.reviews,
+            previewImage: previewImage.url
+
+        }
+        payload.push(spotData)
+        spotsObj['Spots'] = payload
+    // }
+    }
+    res.json(spotsObj)
+
+
+})
 
 
 
