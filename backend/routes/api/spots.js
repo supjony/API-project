72782a1ecf09858f76e314b9d59d3891e31bb78c
Jonathan Sarceno <johnny@Jonathans-MacBook-Air.local>
2023-06-14 12:57:33 -0700

    fogot to add the validate function ok second testing for all of spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,6 +12,57 @@ const user = require('../../db/models/user');
 const router = express.Router();
 
 
+
+
+const validateCreateASpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required.'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('state')
+       .exists({ checkFalsy: true })
+      .withMessage('State is required.'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required.'),
+      check('lat')
+      .exists({checkFalsy: true})
+      .isNumeric()
+      .withMessage('Latitude is not valid'),
+      check('lng')
+      .exists({checkFalsy: true})
+      .isNumeric()
+      .withMessage('Longitude is not valid'),
+      check('name')
+      .exists({checkFalsy: true})
+      .isLength({max: 50})
+      .withMessage("Name must be less than 50 characters"),
+      check('description')
+      .exists({checkFalsy: true})
+      .withMessage("Description is required"),
+      check('price')
+      .exists({checkFalsy: true})
+      .isNumeric()
+      .withMessage("Price per day is required"),
+    handleValidationErrors
+  ];
+
+
+
+
+
+
+// router.get("/", async (req, res, next) => {
+//     const spots = await Spot.findAll()
+//     res.json(spots)
+// })
+
+
+
+
+
 router.get('/current', requireAuth, async (req, res, next) => {
 
     let currentUserLoggedIn = req.user.id
