e88586bced402f382e6645513cfb7d1b23be4321
Jonathan Sarceno <johnny@Jonathans-MacBook-Air.local>
2023-06-14 16:43:25 -0700

    spot image router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
-// const spotImageRouter = require('./spot-images.js');
+const spotImageRouter = require('./spot-images.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -22,7 +22,7 @@ router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
 
-// router.use('/spot-images', spotImageRouter);
+router.use('/spot-images', spotImageRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Spot, SpotImage } = require('../../db/models');
+
+
+const router = express.Router();
+
+
+
+
+
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    let currentUser = req.user.id;
+
+    let spotImage = await SpotImage.findByPk(req.params.imageId);
+
+    if (!spotImage) {
+        return res.status(404).json({
+            "message": "Spot Image could not be found",
+            "statusCode": 404
+          })
+    }
+
+    let spot = await spotImage.getSpot({
+        where: {
+            ownerId: currentUser
+        }
+    })
+
+    if (!spot || spot.id !== spotImage.spotId) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            statusCode: 403
+        })
+    }
+
+    await spotImage.destroy();
+
+    res.status(200).json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+      })
+})
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
